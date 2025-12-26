d9fb04696222181742d0aa4f9dbbbb204bc12e68
Divya <divyayadav210307@gmail.com>
2025-12-26 15:40:49 +0530

    Add support for detecting multiple colors in real-time
    Real-time detection and tracking of multiple colors using OpenCV with bounding box visualization.

--- img.py
+++ img.py
@@ -4,6 +4,7 @@ from PIL import Image
 from this import get_range
 
 orange=[0,165,255]
+blue=[255,0,0]
 c=cv2.VideoCapture(0)
 
 while True:
@@ -11,18 +12,28 @@ while True:
     diff_img=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     
     lowerlimit,upperlimit=get_range(color=orange)
+    lowerlimit2,upperlimit2=get_range(color=blue)
     
     
     mask=cv2.inRange(diff_img,lowerlimit,upperlimit)
+    mask2=cv2.inRange(diff_img,lowerlimit2,upperlimit2)
     
     mask_=Image.fromarray(mask)  #converting to pillow from np array so that it gets easier to get bounding box.
+    mask_2=Image.fromarray(mask2)  #converting to pillow from np array so that it gets easier to get bounding box.
     
     boundingbox=mask_.getbbox()
+    boundingbox2=mask_2.getbbox()
+    
     #print(boundingbox)
     if boundingbox is not None:
         x1,y1,x2,y2=boundingbox
         
         frame=cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),5)
+        
+    if boundingbox2 is not None:
+        x,y,xz,yz=boundingbox2
+        
+        frame=cv2.rectangle(frame,(x,y),(xz,yz),(0,255,0),5)
         
     cv2.imshow('frame',frame)
     
